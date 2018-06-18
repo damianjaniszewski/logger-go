a4b43e44245d68c0e68ec37098841be440a9c2ca
Damian Janiszewski <damian.janiszewski@gmail.com>
2018-06-18 08:34:18 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# logger-go
+
+This is a simple logging helper for golang 
+
+To deploy this application, execute the following commands:
+
+  1. Get logger-go sources:
+
+```
+$ go get github.com/damian-janiszewski/logger-go
+```
+
+  2. Set environment variables before using
+
+```
+SLACKAPI_TOKEN=<slack api token>
+SLACK_CHANNEL=<slack channel>
+```
---
+++ logger.go
@@ -0,0 +1,114 @@
+package logger
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/nlopes/slack"
+)
+
+var debugExists = false
+var debugVerboseExists = false
+
+var logToSlack = false
+var slackAPI *slack.Client
+var slackParams slack.PostMessageParameters
+
+func init() {
+	_, debugExists = os.LookupEnv("DEBUG")
+	_, debugVerboseExists = os.LookupEnv("DEBUGVERBOSE")
+
+	apiToken, apiTokenExists := os.LookupEnv("SLACKAPI_TOKEN")
+	if !apiTokenExists {
+		log.Printf("logger [" + LogErr + "] SLACKAPI_TOKEN env variable not set\n")
+	}
+	if debugVerboseExists {
+		log.Printf("logger ["+LogDebugVerbose+"] slack api token: %s", apiToken)
+	}
+
+	channelName, channelNameExists := os.LookupEnv("SLACK_CHANNEL")
+	if !channelNameExists {
+		log.Printf("logger [" + LogErr + "] SLACK_CHANNEL env variable not set")
+	}
+	if debugVerboseExists {
+		log.Printf("logger ["+LogDebug+"] slack channel: %s", channelName)
+	}
+
+	if apiTokenExists && channelNameExists {
+		logToSlack = true
+
+		slackAPI = slack.New(apiToken)
+		slackParams = slack.PostMessageParameters{}
+		// attachment := slack.Attachment{
+		// 	Pretext: "some pretext",
+		// 	Text:    "some text",
+		// 	// Uncomment the following part to send a field too
+		// 	/*
+		// 		Fields: []slack.AttachmentField{
+		// 			slack.AttachmentField{
+		// 				Title: "a",
+		// 				Value: "no",
+		// 			},
+		// 		},
+		// 	*/
+		// }
+		// params.Attachments = []slack.Attachment{attachment}
+		slackParams.AsUser = true
+		slackParams.Username = "logger"
+	}
+}
+
+// logger levels
+const (
+	LogPanic        = "PANIC"
+	LogFatal        = "FATAL"
+	LogErr          = "ERR"
+	LogWarn         = "WARN"
+	LogInfo         = "INFO"
+	LogDebug        = "DEBUG"
+	LogDebugVerbose = "DEBUGVERBOSE"
+)
+
+// Log formatted messages
+// env variables:
+//   DEBUG: include DEBUG level messages
+//   DEBUGVERBOSE: include DEBUGVERBOSE level messages
+func Log(module string, level string, format string, v ...interface{}) {
+	switch level {
+	case LogPanic:
+		log.Panicf(module+" ["+LogPanic+"] "+format, v...)
+		return
+	case LogFatal:
+		log.Fatalf(module+" ["+LogFatal+"] "+format, v...)
+		return
+	case LogDebug:
+		if debugExists || debugVerboseExists {
+			log.Printf(module+" ["+level+"] "+format, v...)
+		}
+		return
+	case LogDebugVerbose:
+		if debugVerboseExists {
+			log.Printf(module+" ["+level+"] "+format, v...)
+		}
+		return
+	default:
+		log.Printf(module+" ["+level+"] "+format, v...)
+	}
+}
+
+// LogToSlack fomatted messages to SLACK channel
+// env variables:
+//   SLACKAPI_TOKEN: SLACK API application token with permissions to write to specified channel
+//   SLACK_CHANNEL: SLACK channel name
+func LogToSlack(module string, level string, format string, v ...interface{}) {
+	if logToSlack {
+		message := fmt.Sprintf("```"+module+" ["+level+"] "+format+"```", v...)
+		channelID, timestamp, err := slackAPI.PostMessage("logs", message, slackParams)
+		if err != nil {
+			Log("logger", LogErr, "%s logger error: %s", module, err)
+		}
+		Log("logger", LogDebugVerbose, "%s message successfully send to channel %s at %s", module, channelID, timestamp)
+	}
+	Log(module, level, format, v...)
+}
